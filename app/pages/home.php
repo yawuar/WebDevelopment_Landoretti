e80ac63a28b3ddb86d13674acc596156a9dddbe5
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-12-26 16:01:31 +0100

    dev@screen.scss: fixed responsive form

--- app/pages/home.php
+++ app/pages/home.php
@@ -1,17 +1,27 @@
+<!-- Header of Home page -->
 <header class="background">
 
 	<div class="carousel">
+
 			<div class="container blue-overlay">
+
 				<div class="overlay-icon">
+
 					<span class="inner"></span>
+
 				</div>
+
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a.</p>
+
 		</div>
+
 	</div>
+
 	<div class="arrows"></div>
 
 </header>
 
+<!-- how content -->
 <section class="how">
 
 	<div class="container">
@@ -56,36 +66,63 @@
 
 </section>
 
+<!-- Content of popular -->
 <section class="popular">
 
 	<h2>Most popular this week</h2>
 
 	<div class="container">
+
 		<div class="row">
+
 			<div class="col-sm-12 col-md-4">
+
 				<div class="col-md-12 overlay">
+
 					<a href="#">
+
 						<div class="svg-icon"></div>
+
 						<div class="transparent"></div>
+
 					</a>
+
 					<img src="/images/popular/popular-1.jpg" alt="popular-1">
+
 				</div>
+
 				<div class="col-md-12 overlay">
+
 					<a href="#">
+
 						<div class="svg-icon"></div>
+
 						<div class="transparent"></div>
+
 					</a>
+
 					<img src="/images/popular/popular-2.jpg" alt="popular-2">
+
 				</div>
+
 			</div>
+
 			<div class="col-sm-8 col-md-8 overlay">
+
 				<a href="#">
+
 					<div class="svg-icon"></div>
+
 					<div class="transparent"></div>
+
 				</a>
+
 				<img src="/images/popular/popular-3.jpg" alt="popular-3">
+
 			</div>
+
 		</div>
+
 	</div>
 
 </section>
--- app/pages/isearch.php
+++ app/pages/isearch.php
@@ -1,16 +1,165 @@
+<!-- Header of Isearch -->
 <header class="isearch-bg">
+
     <div class="container">
+
         <div class="info pull-right">
+
             <div class="overlay-icon">
-				<span class="inner"></span>
-			</div>
-            <div class="top blue-overlay">
-                <h1>Lorem Ipsum</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. magnis dis parturient montes, nascetur ridiculus mus...</p>
-                <span class="bold">Price:</span> 
-                <span class="euro-size">&euro; 299,99</span>
+
+                <span class="inner"></span>
+                
             </div>
+            
+            <div class="top blue-overlay">
+
+                <h1>Lorem Ipsum</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. magnis dis parturient montes, nascetur ridiculus mus...</p>
+
+                <span class="bold">Price:</span> 
+
+                <span class="euro-size">&euro; 299,99</span>
+
+            </div>
+
             <a href="#" class="bottom">visit auction<span></span></a>
+
+        </div>
+
+    </div>
+
+</header>
+
+<!-- Content of Isearch -->
+<section id="isearch-content">
+    <div class="container">
+        <div class="breadcrumbs">
+            <ul>
+                <li><a href="#">Home ></a></li>
+                <li><a href="#">I search</a></li>
+            </ul>
+        </div>
+
+        <h1>I search</h1>
+
+        <h3>Add a request</h3>
+
+        <form>
+            <div class="row">
+                <div class="col-sm-6 form-group right-marg">
+                    <label for="what">What are you looking for?</label>
+                    <input type="text" class="form-control" name="what" id="what" placeholder="Mona Lisa">
+                </div>
+                <div class="col-sm-6 form-group">
+                    <label for="artist">Artist</label>
+                    <input type="text" class="form-control" name="artist" id="artist" placeholder="Leonardo Da Vinci">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-9 form-group right-marg">
+                    <label for="info">Information about the artwork</label>
+                    <input type="text" class="form-control" name="info" id="info" placeholder="Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore">
+                </div>
+                <div class="col-sm-3 form-group button">
+                    <input type="submit" class="btn" name="btn" id="btn" value="ADMIT REQUEST">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <small id="passwordHelpBlock" class="form-text text-muted">Your request must be approved by the administrator. If your question has been approved, it will appear on our site.</small>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</section>
+
+<!-- Content of Isearch -->
+<section id="isearch-overview">
+    <div class="container">
+        <h3>Overview</h3>
+        <div class="articles">
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
+            <article class="col-sm-12">
+                <div class="info col-sm-9">
+                    <p><span>Search:</span> Mona Lisa, Leonardo Da Vinci</p>
+                    <p><span>Description:</span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    <p><span>Posted:</span> 5 July 2013</p>
+                </div>
+                <div class="button col-sm-3">
+                    <a href="#">I OWN THIS ARTWORK ></a>
+                </div>
+            </article>
         </div>
     </div>
-</header>
+</section>
