6a8e2dbf201e61dd1caf33dc26b53733cb8cdded
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-12-27 14:04:05 +0100

    fixed responsive add auction form

--- app/pages/addauction.php
+++ app/pages/addauction.php
@@ -37,9 +37,33 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-6 form-group right-marg">
+                <div class="col-sm-6 form-group right-marg title">
                     <label for="title">Auction title</label>
                     <input type="text" class="form-control" name="title" id="title" placeholder="Auction title">
+                </div>
+                <div class="col-sm-6 form-group right-marg small-input">
+                    <label for="title">Year</label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="X X X X">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 form-group small-input">
+                    <label for="title">Width</label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="Auction title">
+                </div>
+                <div class="col-sm-4 form-group small-input">
+                    <label for="title">Height</label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="X X X X">
+                </div>
+                <div class="col-sm-4 form-group small-input">
+                    <label for="title">Depth <span>(optional)</span></label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="X X X X">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 form-group big-textarea">
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description"></textarea>
                 </div>
             </div>
         </form>
