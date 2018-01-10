50d8ae92b992f797949fc52c7833420fa5b1f41d
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2018-01-10 13:19:51 +0100

    dev@detail.php: added description div

--- app/pages/detail.php
+++ app/pages/detail.php
@@ -80,7 +80,42 @@
                     </div>
                 </div>
             </div>
-            <div class="row"></div>
+            <div class="row">
+                <div class="col-sm-12 info-auction">
+                    <div class="col-sm-9">
+                        <div class="text">
+                            <h6>Description</h6>
+                            <p>"Dance of Time III" (1979-84) is a stunning bronze and gold patina sculpture<br />
+    by Spanish Contemporary artist Salvador Dalí (Spain, 1904 - 1989), measures 10.43 inches (26.50 cm) and is stamped and numbered from an edition of 350. This sculpture is in perfect condition and is accompanied by documentation of authenticity. As one of the most well known and iconic images seen in Dali’s work, the melted watch has appeared consistently throughout the artist’s oeuvre. As time universally knows no limits, it is perceived as something perpetual- ‘dancing on’ and stopping for no one as exemplified in this ‘Dance of Time III’ sculpture. Meant to echo the fluidity of time, the undulated form of this melted clock captures the surrealistic imagery so beloved and continuously employed by Dali.</p>
+                        </div>
+                        <div class="text">
+                            <h6>Description</h6>
+                            <p>Lorem ipsum dolor sit amet, nec dictum, consectetuer donec tincidunt eget ante libero gravida. Wisi nec nulla, lorem suspendisse lorem id tellus eget, erat tellus, enim at. A purus libero amet vitae accumsan scelerisque, quis orci sed ornare pulvinar, luctus mauris turpis mollis ut vel eget, id massa non cursus felis. Sed et mus non non augue tortor. Pede id massa, ante rhoncus et at ad, fringilla vulputate dapibus nunc, iaculis sollicitudin accumsan non, hac vehicula hymenaeos. Felis aliquet donec vitae. Autem ante, sollicitudin feugiat amet viverra quae risus, dui dis semper ac non, mi curabitur amet. Bibendum nunc fusce aliquam ante tempus, imperdiet magna.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div>
+                            <div class="artist">
+                                <p class="title">Artist</p>
+                                <p>Salvador Dali</p>
+                                <p>Spanish</p>
+                                <p>1904 - 1989</p>
+                            </div>
+                            <div class="dimensions">
+                                <p class="title">Dimensions</p>
+                                <p>10.43 x 10.43 x 10.44 cm</p>
+                            </div>
+                            <div class="color">
+                                <p class="title">Color</p>
+                                <p>Bronze, patinated bronze and gold</p>
+                            </div>
+                            <div class="ask">
+                                <a href="#">Ask a question<br /> about this auction</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
