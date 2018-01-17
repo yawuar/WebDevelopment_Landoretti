a0dad9272107d98986072dd1526826d5c009227d
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2018-01-17 12:56:55 +0100

    added cache control in header.php

--- dist/includes/header.php
+++ dist/includes/header.php
@@ -36,6 +36,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+	<meta http-equiv="Cache-control" content="public">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Landoretti</title>
