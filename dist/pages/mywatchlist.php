5b9cd297a184ac7370f55f30e5f07aba1ffeb6ab
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2018-01-18 16:58:05 +0100

    fixed responsive table in mywatchlist.php

--- dist/pages/mywatchlist.php
+++ dist/pages/mywatchlist.php
@@ -27,194 +27,196 @@
                     <li><a href="">ended(3)</a></li>
                 </ul>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Auction details</th>
-                        <th>Estimated price</th>
-                        <th>End date</th>
-                        <th>Remaining time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/radio.jpg" alt="Radio">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+            <div class="table-overflow">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Auction details</th>
+                            <th>Estimated price</th>
+                            <th>End date</th>
+                            <th>Remaining time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/radio.jpg" alt="Radio">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/clock.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/clock.jpg" alt="Blue Man">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/lady.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/body.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/lady.jpg" alt="Blue Man">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/elephant.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/faces.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/building.jpg" alt="Blue Man">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="image">
-                                <img src="../images/myauctions/blueman.jpg" alt="Blue Man">
-                            </div>
-                            <div class="info">
-                                <div>
-                                    <h3>Dance of Time III</h3>
-                                    <a href="#">1979, Salvador Dali</a>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/body.jpg" alt="Blue Man">
                                 </div>
-                            </div>
-                        </td>
-                        <td>&euro; 8.900</td>
-                        <td>September 09, 2013 13:00 p.m. (EST)</td>
-                        <td>25d 14u 44m</td>
-                    </tr>
-                </tbody>
-            </table>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/ocean.jpg" alt="Blue Man">
+                                </div>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/elephant.jpg" alt="Blue Man">
+                                </div>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/faces.jpg" alt="Blue Man">
+                                </div>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/vynil.jpg" alt="Blue Man">
+                                </div>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="image">
+                                    <img src="../images/myauctions/blueCouple.jpg" alt="Blue Man">
+                                </div>
+                                <div class="info">
+                                    <div>
+                                        <h3>Dance of Time III</h3>
+                                        <a href="#">1979, Salvador Dali</a>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>&euro; 8.900</td>
+                            <td>September 09, 2013 13:00 p.m. (EST)</td>
+                            <td>25d 14u 44m</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </section>
