b090333193a739a186b5c580ba25b55324e826d9
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2018-01-19 17:03:22 +0100

    changed gulpfile to add fonts folder

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task('includes', function () {
   gulp.src('app/pages/*.php').pipe(gulp.dest('dist/pages'));
   gulp.src('app/*.php').pipe(gulp.dest('dist'));
   gulp.src('app/.htaccess').pipe(gulp.dest('dist'));
-  gulp.src('app/robots.txt').pipe(gulp.dest('dist'));
+  gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 });
 // Cleaning 
 gulp.task('clean', function() {
